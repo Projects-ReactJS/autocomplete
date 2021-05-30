e84d96eb19cf30cc3615fa75d1c9c4185982d25c
balajimurugesan2016 <balajimurugesan.2016@gmail.com>
2021-05-30 20:26:24 +0530

    Fixing jest issues

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import App from './App';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
 
 
 /*test('renders learn react link', () => {
